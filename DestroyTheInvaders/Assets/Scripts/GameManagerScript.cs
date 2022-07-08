49e2326e142817ac280431d4fe8cfdfedd74a195
Ahmet Burçoğlu <ahmetburcoglu2@gmail.com>
2022-07-08 04:15:14 +0300

    Hero auto move dynamics added and calibrated.

---
+++ DestroyTheInvaders/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    public SpawnAreaManager spawnAreaManager;
+    public GameObject spawnAreaBorders;
+    public void Game_Start()
+    {
+        StartCoroutine(GameStartPRO());
+    }
+    IEnumerator GameStartPRO()
+    {
+        spawnAreaBorders.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        foreach (GameObject SpawnArea in spawnAreaManager.spawnAreas)
+        {
+            if (SpawnArea.GetComponent<SpawnAreaProperties>().isFull)
+            {
+                Destroy(SpawnArea.GetComponent<SpawnAreaProperties>().heroGameObject.GetComponent<MoveObjectScript>());
+                SpawnArea.GetComponent<SpawnAreaProperties>().heroGameObject.AddComponent<Hero_AutoMove>();
+                SpawnArea.GetComponent<SpawnAreaProperties>().heroGameObject.GetComponent<Rigidbody>().isKinematic = false;
+            }
+            Destroy(SpawnArea.GetComponent<SpawnAreaProperties>());
+            Destroy(SpawnArea.GetComponent<SpawnAreaHeroMagnetScript>());
+            SpawnArea.GetComponent<MeshRenderer>().enabled = false;
+            SpawnArea.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+    public void HidePanel(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+    
+}
---
+++ DestroyTheInvaders/Assets/Scripts/Hero_Auto/Hero_AutoMove.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero_AutoMove : MonoBehaviour
+{
+    GameObject TEST;
+    private bool targetBoundsInteracted = false;
+ 
+    void Start()
+    {
+        TEST = GameObject.Find("TEST");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            targetBoundsInteracted = true;
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            targetBoundsInteracted = false;
+        }
+        
+    }
+
+    void Update()
+    {
+        if (!targetBoundsInteracted)
+        {
+            this.transform.LookAt(TEST.transform, new Vector3(0f, 1f, 0f));
+            this.transform.localRotation = Quaternion.Euler(0f, this.transform.localRotation.eulerAngles.y, this.transform.localRotation.eulerAngles.z);
+            this.transform.position = Vector3.MoveTowards(this.transform.position, TEST.transform.position, Time.deltaTime);
+        }
+    }
+}
